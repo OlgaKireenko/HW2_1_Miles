2dce2126e2501d35dae79ae1cc4c00d871bc90cb
Kireenko Olga <kireenko86.osil@mail.ru>
2021-04-07 13:32:45 +0500

    add cod

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {public static void main(String[] args) {
+     long ticketPrice = 205555;
+     int rubValue = 20;
+     int bonus = 1;
+     long finalBonus = ticketPrice/rubValue*bonus;
+     System.out.println(finalBonus);
+}
+}
